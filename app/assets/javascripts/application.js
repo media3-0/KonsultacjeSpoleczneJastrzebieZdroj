df9a77b9934ae96dd95eaaef5b6f1b791f7cbea6
Jakub Syty <jakub.nekro@gmail.com>
2015-03-30 09:12:47 +0200

    ckeditor - poprawki

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require ckeditor/init
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require ckeditor/init
 //= require_tree .
 
