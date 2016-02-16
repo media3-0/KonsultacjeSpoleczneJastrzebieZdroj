25a5df9ec082b84ddd8f065b61fb37c859da65f5
Nekromancer <jakub.nekro@gmail.com>
2016-02-16 11:18:52 +0100

    Reminder mailing

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
-  default from: 'konsultacje.jastrzebie@gmail.com'
+  default from: 'noreply@konsultacje.jastrzebie.pl'
 
   def report_email(comment)
     @id = comment.id
@@ -7,5 +7,12 @@ class UserMailer < ActionMailer::Base
     @user_name = comment.user.name
     @url = admin_consultation_comment_url(comment)
     mail(to: 'konsultacje.jastrzebie@gmail.com', subject: "Zgłoszenie komentarza id: #{comment.id}")
+  end
+
+  def send_reminder
+  	users = User.where(sign_in_count: 0)
+  	users.each do |user|
+  		mail(to: user.email, subject: 'Zapraszamy na platformę konsultacje.jastrzebie.pl')
+  	end
   end
 end
